d33b6e40a8d544c598dc219460991cad2017bdb6
Asm-Def <woojson@zju.edu.cn>
2023-02-20 18:32:08 +0800

    [Feat]: Change JS module format to support Kaggle Notebooks (#6)

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ echo $cur_dir
 echo $file_dir
 cd $file_dir/graphic-walker
 yarn && yarn build
-cp packages/graphic-walker/dist/graphic-walker.umd.js ../pygwalker/templates/
+cp packages/graphic-walker/dist/graphic-walker.*.js ../pygwalker/templates/
 cd $cur_dir
--- pygwalker/base.py
+++ pygwalker/base.py
@@ -11,8 +11,8 @@ gwalker_js = None
 def gwalker_script():
     global gwalker_js
     if gwalker_js is None:
-        with open(os.path.join(HERE, 'templates', 'graphic-walker.umd.js'), 'r') as f:
-            gwalker_js = "const process={env:{NODE_ENV:\"production\"} };" + f.read()
+        with open(os.path.join(HERE, 'templates', 'graphic-walker.iife.js'), 'r') as f:
+            gwalker_js = "const exports={};const process={env:{NODE_ENV:\"production\"} };" + f.read()
     return gwalker_js
         
 
