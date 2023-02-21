363d880da77f46474bb3af037f88da8430e74728
Asm-Def <woojson@zju.edu.cn>
2023-02-22 04:24:54 +0800

    feat:support DataBricks (#19)

--- pygwalker/templates/walk.js
+++ pygwalker/templates/walk.js
@@ -1,4 +1,4 @@
-const gw_id = "gwalker-{{ gwalker.id }}";
-let props = JSON.parse(`{{ gwalker.props }}`);
+var gw_id = "gwalker-{{ gwalker.id }}";
+var props = JSON.parse(`{{ gwalker.props }}`);
 console.log(props, gw_id);
 GraphicWalker.GWalker(props, gw_id);
