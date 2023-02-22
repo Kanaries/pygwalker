f9942fe3bbf5955a53ad6f3d0237f1af37531ed3
Asm.Def <woojson@zju.edu.cn>
2023-02-23 03:36:09 +0800

    fix:error with special characters in DataFrame

--- pygwalker/templates/walk.js
+++ pygwalker/templates/walk.js
@@ -1,4 +1,4 @@
 var gw_id = "gwalker-{{ gwalker.id }}";
-var props = JSON.parse(`{{ gwalker.props }}`);
+var props = {{ gwalker.props }};
 console.log(props, gw_id);
 GraphicWalker.GWalker(props, gw_id);
