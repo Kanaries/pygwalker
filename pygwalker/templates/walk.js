864abf9248d54b5d83e7cd352784d9ce7a004cd4
Asm-Def <willywu@foxmail.com>
2023-02-27 22:19:31 +0800

    Revert "feat:dump data in tight mode to reduce ipynb file size"
    This reverts commit 0fcb7c4c1cdc58e312109023460f99a90fa99fc5.

--- pygwalker/templates/walk.js
+++ pygwalker/templates/walk.js
@@ -1,5 +1,4 @@
 var gw_id = "gwalker-{{ gwalker.id }}";
 var props = {{ gwalker.props }};
-props.dataSource = props.dataSource.data.map(record => props.dataSource.columns.map((col, i) => ({ [col]: i })));
 console.log(props, gw_id);
 GraphicWalker.GWalker(props, gw_id);
