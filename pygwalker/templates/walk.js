c21c5d0952461af991700b4b042bab2853864faf
Asm.Def <woojson@zju.edu.cn>
2023-02-27 20:17:20 +0800

    feat:dump data in tight mode to reduce ipynb file size

--- pygwalker/templates/walk.js
+++ pygwalker/templates/walk.js
@@ -1,6 +1,7 @@
 try{
 var gw_id = "gwalker-{{ gwalker.id }}";
 var props = {{ gwalker.props }};
+props.dataSource = props.dataSource.data.map(record => props.dataSource.columns.map((col, i) => ({ [col]: i })));
 console.log(props, gw_id);
 GraphicWalker.GWalker(props, gw_id);
 }catch(e){ console.error(e); }
