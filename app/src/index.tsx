eaaf975a301c05305d457051dfb0eb7d7d1616c6
Asm.Def <woojson@zju.edu.cn>
2023-03-14 05:17:24 +0800

    to:feat:reminder to update

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { GraphicWalker } from '@kanaries/graphic-walker'
-import type { IGWProps } from '@kanaries/graphic-walker/dist/App'
-import type { IGlobalStore } from '@kanaries/graphic-walker/dist/store'
+import type { IGWProps } from '../../graphic-walker/packages/graphic-walker/dist/App'
+import type { IGlobalStore } from '../../graphic-walker/packages/graphic-walker/dist/store'
 // import type { IGWProps } from 'gwalker/App'
 
 import Options from './components/options';
 import { IAppProps } from './interfaces';
+import type { IStoInfo } from '../../graphic-walker/packages/graphic-walker/dist/utils/save';
 
-const App: React.FC<IAppProps> = (props) => {
-  const ref = React.useRef<IGlobalStore | null>(null);
-  return (<React.StrictMode>
-    <Options {...props} storeRef={ref} />
-    <GraphicWalker {...props} storeRef={ref} />
-  </React.StrictMode>);
+/** App does not consider props.storeRef */
+const App: React.FC<IAppProps> = (propsIn) => {
+  const storeRef = React.useRef<IGlobalStore|null>(null);
+  const props: IAppProps = {...propsIn};
+  if (!props.storeRef?.current) {
+    props.storeRef = storeRef;
+  }
+
+  useEffect(() => {
+    if (props.visSpec && storeRef.current) {
+      // TODO: DataSet and DataSource ID
+      const specList = JSON.parse(props.visSpec);
+      storeRef?.current?.vizStore?.importStoInfo({
+        dataSources: [{
+          id: 'dataSource-0',
+          data: props.dataSource,
+        }],
+        datasets: [{
+          id: 'dataset-0',
+          name: 'DataSet', rawFields: props.rawFields, dsId: 'dataSource-0',
+        }],
+        specList: specList,
+      } as IStoInfo);
+    }
+  }, []);
+  return (
+    <React.StrictMode>
+      <GraphicWalker {...props} />
+      <Options {...props} />
+    </React.StrictMode>
+  );
 }
 
 function GWalker(props: IAppProps, id: string) {
