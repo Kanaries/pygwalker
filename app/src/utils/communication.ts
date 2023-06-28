26fc3c0ed77a5ca219bd05f928a7ddf19e6c7362
longxiaofei <libra.19951002@gmail.com>
2023-06-28 12:49:35 +0800

    feat: add communications module

---
+++ app/src/utils/communication.ts
@@ -0,0 +1,83 @@
+import { v4 as uuidv4 } from 'uuid';
+
+interface ICommunication {
+    sendMsg: (action: string, data: any) => Promise<any>;
+    registerEndpoint: (action: string, callback: (data: any) => any) => void;
+    sendMsgAsync: (action: string, data: any, rid: string | null) => void;
+}
+
+const getSignalName = (rid: string) => {
+    return `hacker-comm-pyg-done-signal-${rid}`;
+}
+
+const initCommunication = (gid: string) => {
+    const document = window.parent.document;
+    const htmlText = document.getElementsByClassName(`hacker-comm-pyg-html-store-${gid}`)[0].childNodes[1] as HTMLInputElement;
+    const kernelText = document.getElementsByClassName(`hacker-comm-pyg-kernel-store-${gid}`)[0].childNodes[1] as HTMLInputElement;;
+
+    const endpoints = new Map<string, (data: any) => any>();
+    const bufferMap = new Map<string, any>();
+
+    const onMessage = (msg: string) => {
+        const data = JSON.parse(msg);
+        const action = data.action;
+        if (action === "finish_request") {
+            bufferMap.set(data.rid, data.data);
+            document.dispatchEvent(new CustomEvent(getSignalName(data.rid)));
+            return
+        }
+        const callback = endpoints.get(action);
+        if (callback) {
+            const resp = callback(data.data) ?? {};
+            sendMsgAsync("finish_request", resp, data.rid);
+        }
+    }
+
+    const sendMsg = async(action: string, data: any) => {
+        const rid = uuidv4();
+        const promise = new Promise<any>((resolve, reject) => {
+            sendMsgAsync(action, data, rid);
+            setTimeout(() => {
+                reject(new Error("get result timeout"))
+            }, 10000);
+            document.addEventListener(getSignalName(rid), (_) => {
+                resolve(bufferMap.get(rid));
+            });
+        });
+
+        return promise;
+    }
+
+    const sendMsgAsync = (action: string, data: any, rid: string | null) => {
+        rid = rid ?? uuidv4();
+        const event = new Event("input", { bubbles: true })
+        kernelText.value = JSON.stringify({ rid: rid, action, data });
+        kernelText.dispatchEvent(event);
+    }
+
+    const registerEndpoint = (action: string, callback: (data: any) => any) => {
+        endpoints.set(action, callback);
+    }
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === "attributes") {
+                onMessage(htmlText.value)
+            }
+        })
+    })
+
+    observer.observe(htmlText, {
+        attributes: true,
+        attributeFilter: ["placeholder"],
+    })
+
+    return {
+        sendMsg,
+        registerEndpoint,
+        sendMsgAsync,
+    }
+}
+
+export type { ICommunication };
+export default initCommunication;
---
+++ pygwalker/communications/base.py
@@ -0,0 +1,34 @@
+from typing import Any, Callable, Dict
+
+
+class BaseCommunication:
+    """
+    Base class for communication
+    message format:
+    {
+        "rid": "xxx",
+        "action": "xxx",
+        "data": {}
+    }
+    """
+    _endpoint_map = {}
+
+    def send_msg_async(self, action: str, data: Dict[str, Any]):
+        raise NotImplementedError
+
+    def send_msg(self, action: str, data: Dict[str, Any]):
+        raise NotImplementedError
+
+    def _receive_msg(self, action: str, data: Dict[str, Any]) -> Dict[str, Any]:
+        handler_func = self._endpoint_map.get(action, None)
+        if handler_func is None:
+            return {"success": False, "message": f"Unknown action: {action}"}
+        try:
+            data = handler_func(data)
+            return {"success": True, "data": data}
+        except Exception as e:
+            return {"success": False, "message": str(e)}
+
+    @classmethod
+    def register(cls, endpoint: str, func: Callable[[Dict[str, Any]], Any]):
+        cls._endpoint_map[endpoint] = func
