f630f6f1b81b14164452ae3ec11c8ca03d3ab09c
longxiaofei <libra.19951002@gmail.com>
2023-07-11 12:12:16 +0800

    feat: add save shortcut

--- app/src/tools/saveTool.tsx
+++ app/src/tools/saveTool.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNotification } from "../notify";
 import communicationStore from "../store/communication"
 import { encodeSpec } from "../utils/graphicWalkerParser"
@@ -10,6 +10,12 @@ import type { IAppProps } from '../interfaces';
 import type { IGWHandler } from '@kanaries/graphic-walker/dist/interfaces';
 import type { ToolbarButtonItem } from "@kanaries/graphic-walker/dist/components/toolbar/toolbar-button"
 import type { IGlobalStore } from '@kanaries/graphic-walker/dist/store'
+
+function saveJupyterNotebook() {
+    const rootDocument = window.parent.document;
+    rootDocument.body.dispatchEvent(new KeyboardEvent('keydown', {key:'s', keyCode: 83, metaKey: true}));
+    rootDocument.body.dispatchEvent(new KeyboardEvent('keydown', {key:'s', keyCode: 83, ctrlKey: true}));
+}
 
 export function hidePreview(id: string) {
     setTimeout(() => {
@@ -37,35 +43,53 @@ export function getSaveTool(
         }, 500);
     }
 
+    const onClick = async () => {
+        if (props.specType !== "json_file") {
+            notify({
+                type: "warning",
+                title: "Tips",
+                message: "spec params is not 'json_file', save is not supported.",
+            }, 4_000)
+            return
+        }
+        if (saving) return;
+        setSaving(true);
+
+        // if exportChart is undefined, it means that the chart is not reload, so we think dont need to save.
+        if (gwRef.current?.exportChart === undefined) {
+            saveSuccess();
+            return;
+        }
+        const chartData = await gwRef.current?.exportChart!("data-url");
+        await communicationStore.comm?.sendMsg("render_preview_image", chartData);
+        hidePreview(props.id);
+        await communicationStore.comm?.sendMsg("update_vis_spec", {
+            "content": encodeSpec(storeRef.current?.vizStore.exportViewSpec()!),
+        });
+        saveSuccess();
+        saveJupyterNotebook();
+    }
+
+    useEffect(() => {
+        let locker = false;
+        document.addEventListener("keydown", (event) => {
+            if ((event.metaKey || event.ctrlKey) && event.key === 's') {
+                event.preventDefault();
+                if (locker) return;
+                locker = true;
+                onClick().then(() => {
+                    locker = false;
+                });
+            }
+        });
+    }, [])
+
     return {
         key: "save",
         label: "save",
         icon: (iconProps?: any) => {
             return saving ? <LoadingIcon width={36} height={36} /> : <DocumentTextIcon {...iconProps} />
         },
-        onClick: async() => {
-            if (props.specType !== "json_file") {
-                notify({
-                    type: "warning",
-                    title: "Tips",
-                    message: "spec params is not 'json_file', save is not supported.",
-                }, 4_000)
-                return
-            }
-            setSaving(true);
-
-            // if exportChart is undefined, it means that the chart is not reload, so we think dont need to save.
-            if (gwRef.current?.exportChart === undefined) {
-                saveSuccess();
-                return;
-            }
-            const chartData = await gwRef.current?.exportChart!("data-url");
-            await communicationStore.comm?.sendMsg("render_preview_image", chartData);
-            hidePreview(props.id);
-            await communicationStore.comm?.sendMsg("update_vis_spec", {
-                "content": encodeSpec(storeRef.current?.vizStore.exportViewSpec()!),
-            });
-            saveSuccess();
-        }
+        onClick: onClick,
     }
 }
