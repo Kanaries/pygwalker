6c9257aef6f4f722316175ec46de2ce6c7ad496f
longxiaofei <libra.19951002@gmail.com>
2023-07-14 18:01:12 +0800

    chore: bump to v0.2.0a2

--- pygwalker/__init__.py
+++ pygwalker/__init__.py
@@ -3,7 +3,7 @@ import logging
 from pygwalker.utils.randoms import rand_str as __rand_str
 from pygwalker_utils.config import get_config as __get_config
 
-__version__ = "0.2.0a1"
+__version__ = "0.2.0a2"
 __hash__ = __rand_str()
 
 from pygwalker.api.walker import walk
