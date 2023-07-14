d666e789b3f798dad811046b3ef179ca800d0d29
longxiaofei <libra.19951002@gmail.com>
2023-07-14 18:00:35 +0800

    style: modify tips text

--- pygwalker/services/tip_tools.py
+++ pygwalker/services/tip_tools.py
@@ -5,7 +5,7 @@ from pygwalker.utils.display import display_html
 
 WIDGETS_TIPS = """
 <div style="">
-If you are using pygwalker in a local notebook (not jupyter lab) environment, please run `pip install "pygwalker[notebook]" --pre` to ensure that pygwalker can be used normally.(close after 30 seconds)
+If you are using pygwalker on Jupyter and it can't display properly, please execute code to fix it: `pip install "pygwalker[notebook]" --pre`.(close after 15 seconds)
 <div>
 """
 
@@ -27,5 +27,5 @@ class TipOnStartTool:
         display_html(self.tips, slot_id=self.slot_id)
 
     def hide(self):
-        time.sleep(30)
+        time.sleep(15)
         display_html("", slot_id=self.slot_id)
