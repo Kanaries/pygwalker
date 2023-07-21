f908d3407edf7e47304eb2aa15bd4d49dc5b81ac
longxiaofei <libra.19951002@gmail.com>
2023-07-21 11:03:25 +0800

    fix: set pygwalker logger

--- pygwalker/__init__.py
+++ pygwalker/__init__.py
@@ -1,3 +1,8 @@
+from pygwalker.utils.log import init_logging
+
+init_logging()
+
+# pylint: disable=wrong-import-position
 import logging
 
 from pygwalker.utils.randoms import rand_str as __rand_str
@@ -11,12 +16,8 @@ from pygwalker.api.gwalker import GWalker
 from pygwalker.api.html import to_html
 from pygwalker.data_parsers.base import FieldSpec
 
-
-logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-
-
 if __get_config('privacy')[0] == 'offline':
-    logging.info("Running in offline mode. There might be newer releases available. Please check at https://github.com/Kanaries/pygwalker or https://pypi.org/project/pygwalker.")
+    logging.getLogger(__name__).info("Running in offline mode. There might be newer releases available. Please check at https://github.com/Kanaries/pygwalker or https://pypi.org/project/pygwalker.")
 else:
     from pygwalker.services.check_update import check_update
     check_update()
