8d3977f46fa910a65b8fe1521696991f16cf1422
longxiaofei <libra.19951002@gmail.com>
2023-07-27 18:29:10 +0800

    feat: use new fid encode

--- pygwalker/services/fname_encodings.py
+++ pygwalker/services/fname_encodings.py
@@ -1,8 +1,34 @@
-import base64
+from math import ceil
 
 
-def fname_encode(fname: str):
-    """Encode fname in base64
+def base36encode(s: str) -> str:
+    """Converts an string to a base36 string."""
+    alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    number = int.from_bytes(s.encode(), "big")
+
+    if not isinstance(number, int):
+        raise TypeError('number must be an integer')
+
+    base36 = ''
+
+    if 0 <= number < len(alphabet):
+        return alphabet[number]
+
+    while number != 0:
+        number, i = divmod(number, len(alphabet))
+        base36 = alphabet[i] + base36
+
+    return base36
+
+
+def base36decode(s: str) -> str:
+    """Converts a base36 string to an string."""
+    number = int(s, 36)
+    return number.to_bytes(ceil(number.bit_length() / 8), "big").decode()
+
+
+def fname_encode(fname: str) -> str:
+    """Encode fname in base32
 
     Args:
         - fname (str): Suppose to be str
@@ -10,8 +36,9 @@ def fname_encode(fname: str):
     Returns:
         str
     """
-    return base64.b64encode(bytes(str(fname), 'utf-8')).decode()
+    return "GW_" + base36encode(fname)
 
 
-def fname_decode(fname: str):
-    return base64.b64decode(str(fname).encode()).decode().rsplit('_', 1)[0]
+def fname_decode(encode_fname: str) -> str:
+    """Decode fname in base32"""
+    return base36decode(encode_fname[3:]).rsplit('_', 1)[0]
