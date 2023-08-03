931ba6b0450f4e5603d7c27e5627f8b773e5f00f
longxiaofei <libra.19951002@gmail.com>
2023-08-03 12:04:14 +0800

    fix: lack psutil dependencies

--- pygwalker/utils/execute_env_check.py
+++ pygwalker/utils/execute_env_check.py
@@ -6,7 +6,10 @@ def check_convert() -> bool:
     """
     Check if the current process is a jupyter-nbconvert process.
     """
+    if psutil.Process().parent() is None:
+        return False
     cmd_list = psutil.Process().parent().cmdline()
     for cmd in cmd_list:
         if re.search(r"jupyter-nbconvert", cmd):
             return True
+    return False
