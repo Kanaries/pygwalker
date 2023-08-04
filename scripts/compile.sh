25c782a892fd76b5716dd13c197e136306121d53
longxiaofei <libra.19951002@gmail.com>
2023-08-04 15:15:15 +0800

    chore: remove customer graphic-walker submodule

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,7 +4,6 @@ file_dir=$(dirname $(dirname $0) )
 echo $cur_dir
 echo $file_dir
 APP=$file_dir/app
-GW=$file_dir/graphic-walker/packages/graphic-walker
 
 (cd $file_dir && if ! command -v yarn; then npm install -g yarn; fi) && \
 (cd $APP && yarn && yarn build)
