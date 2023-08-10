22e8259de6a931b8e6ca24befe5b6e63e2f7781c
longxiaofei <libra.19951002@gmail.com>
2023-08-10 10:35:18 +0800

    fix: fix polars bug in use_kernel_calc

--- pygwalker/services/calculation.py
+++ pygwalker/services/calculation.py
@@ -9,9 +9,11 @@ def get_datas_from_dataframe(df: DataFrame, sql: str) -> List[Dict[str, Any]]:
     """
     Get datas from dataframe by sql(duckdb).
     """
-    mid_table_name = "__mid_df"
+    # __mid_df as as an intermediate variable of duckdb read dataframe.
+    __mid_df = df
     sql = sql.encode('utf-8').decode('unicode_escape')
-    result = duckdb.query_df(df, mid_table_name, sql)
+    result = duckdb.query(sql)
+
     return [
         dict(zip(result.columns, row))
         for row in result.fetchall()
