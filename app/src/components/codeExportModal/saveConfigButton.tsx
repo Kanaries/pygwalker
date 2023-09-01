653ba61b58a82be7051cb76cfbb019661d26e0cb
longxiaofei <libra.19951002@gmail.com>
2023-09-01 13:31:33 +0800

    feat: delete export config as config

--- app/src/components/codeExportModal/saveConfigButton.tsx
+++ app/src/components/codeExportModal/saveConfigButton.tsx
@@ -2,8 +2,6 @@
 import React, { Fragment, useState, useEffect, useMemo } from "react";
 import { download } from "../../utils/save";
 import { Menu, Transition } from "@headlessui/react";
-import { getToken } from "@kanaries/auth-wrapper";
-import { checkUploadPrivacy } from "../../utils/userConfig";
 import commonStore from "../../store/common";
 
 import type { IVisSpec } from '@kanaries/graphic-walker/dist/interfaces'
@@ -39,45 +37,6 @@ const saveConfigButton: React.FC<IDsaveConfigButtonProps> = (props) => {
         download(pygConfig, "config.json", "text/plain");
         props.setPygCode(code);
         setSaving(false);
-    }
-
-    const saveAsServer = async() => {
-        setSaving(true);
-        let token: string;
-        try {
-            token = await getToken();
-        } catch (e) {
-            props.setTips("get token timeout, please try again later or login again.")
-            setSaving(false);
-            return
-        }
-        try {
-            const resp = await fetch(
-                "https://i4rwxmw117.execute-api.us-east-1.amazonaws.com/default/pygwalker-config",
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        token: token,
-                        params: {
-                            "config_json": pygConfig,
-                        }
-                    })
-                }
-              )
-              const respJson = await resp.json();
-              if (respJson["code"] !== 0) {
-                props.setTips("auth failed, if needed export as config id, please login again.")
-                return
-              }
-              const configId = respJson["data"]["config_id"]
-              const code = sourceCode.replace("____pyg_walker_spec_params____", configId)
-              props.setPygCode(code);
-              props.setTips("generate config id success!");
-        } catch (e) {
-            props.setTips("unknown error, please try again later.")
-        } finally {
-            setSaving(false);
-        }
     }
 
     useEffect(() => {
@@ -119,25 +78,6 @@ const saveConfigButton: React.FC<IDsaveConfigButtonProps> = (props) => {
                             >
                                 File
                             </button>
-                        </Menu.Item>
-                        <Menu.Item>
-                            <div className="group relative w-max">
-                                <button
-                                    className="group flex w-full items-center rounded-md px-2 py-2 text-sm disabled:opacity-50"
-                                    onClick={saveAsServer}
-                                    disabled={!checkUploadPrivacy() || !commonStore.showCloudTool}
-                                >
-                                    Config Id
-                                </button>
-                                {
-                                    (!checkUploadPrivacy() || !commonStore.showCloudTool) &&
-                                    <span
-                                        className="absolute w-60 top-10 scale-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100"
-                                    >
-                                        you need set your privacy to meta or any to use this feature and set `show_cloud_tool=True`.
-                                    </span>
-                                }
-                            </div>
                         </Menu.Item>
                     </div>
                 </Menu.Items>
