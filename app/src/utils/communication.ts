07a8632c9560275e3e81f65c2bfb3bc1c7a3e135
longxiaofei <libra.19951002@gmail.com>
2023-09-20 23:34:07 +0800

    fix: temporary fix in streamlit cloud

--- app/src/utils/communication.ts
+++ app/src/utils/communication.ts
@@ -112,7 +112,13 @@ const initJupyterCommunication = (gid: string) => {
 }
 
 const initStreamlitCommunication = (gid: string) => {
-    const url = `/_stcore/_pygwalker/comm/${gid}`
+    // temporary solution in streamlit could
+    const domain = window.document.location.host.split(".").slice(-2).join('.');
+    if (domain === "streamlit.app") {
+        const url = `/~/+/_stcore/_pygwalker/comm/${gid}`
+    } else {
+        const url = `/_stcore/_pygwalker/comm/${gid}`
+    }
 
     const sendMsg = async(action: string, data: any, timeout: number = 30_000) => {
         const timer = setTimeout(() => {
