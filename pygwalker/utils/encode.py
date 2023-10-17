0e451bb5a2fea1735c710035a216aa4b37f6ad55
longxiaofei <libra.19951002@gmail.com>
2023-10-17 16:53:37 +0800

    feat: convert datetime to number for millisecond timestamp

--- pygwalker/utils/encode.py
+++ pygwalker/utils/encode.py
@@ -1,8 +1,12 @@
 import json
+from datetime import datetime
 
 
 class DataFrameEncoder(json.JSONEncoder):
     def default(self, o):
+        if isinstance(o, datetime):
+            return int(o.timestamp() * 1000)
+
         try:
             return str(o)
         except TypeError:
