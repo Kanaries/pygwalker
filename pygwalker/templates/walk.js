b3fd38a40ba253f8aa9a6d4102f9f77742753c38
longxiaofei <libra.19951002@gmail.com>
2023-10-27 13:43:23 +0800

    feat: store local user id

--- pygwalker/templates/walk.js
+++ pygwalker/templates/walk.js
@@ -3,7 +3,6 @@ var props = {{ gwalker.props }};
 console.log(PyGWalkerApp, props, gw_id);
 
 try{
-    window.__GW_HASH=props.hashcode;
     window.__GW_VERSION=props.version;
     PyGWalkerApp.GWalker(props, gw_id);
 } catch(e) {
--- pygwalker/utils/randoms.py
+++ pygwalker/utils/randoms.py
@@ -1,6 +1,14 @@
+from datetime import datetime, timezone
 import random
 import string
 
 
 def rand_str(n: int = 8, options: str = string.ascii_letters + string.digits) -> str:
     return ''.join(random.sample(options, n))
+
+
+def generate_hash_code() -> str:
+    now_time = int(datetime.now(timezone.utc).timestamp() * 1000 * 1000)
+    pre_str = format(now_time, "x")
+    pre_str = pre_str.zfill(16)
+    return pre_str + rand_str(16)
