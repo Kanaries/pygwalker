63f4c23f7c62aec46ec645063b9e6a72f80a47b8
longxiaofei <libra.19951002@gmail.com>
2023-11-13 18:51:45 +0800

    feat: optimize formatted data

--- pygwalker/utils/encode.py
+++ pygwalker/utils/encode.py
@@ -1,14 +1,20 @@
 import json
 from datetime import datetime
+from decimal import Decimal
 
 
 class DataFrameEncoder(json.JSONEncoder):
+    """JSON encoder for DataFrame"""
     def default(self, o):
         if isinstance(o, datetime):
             return int(o.timestamp() * 1000)
+        if isinstance(o, Decimal):
+            return float(o)
 
         try:
-            return str(o)
-        except TypeError:
-            pass
-        return json.JSONEncoder.default(self, o)
+            return json.JSONEncoder.default(self, o)
+        except Exception:
+            try:
+                return str(o)
+            except TypeError:
+                return None
