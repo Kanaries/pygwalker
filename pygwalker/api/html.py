3151067116455f53a38a6bcf04fc8d6ff6271991
longxiaofei <libra.19951002@gmail.com>
2024-03-08 10:54:35 +0800

    feat: add new api to_chart_html

--- pygwalker/api/html.py
+++ pygwalker/api/html.py
@@ -1,11 +1,14 @@
-from typing import Union, Dict, Optional
+from typing import Union, Dict, Optional, Any
 import logging
 import traceback
 
 from typing_extensions import Literal
 
 from .pygwalker import PygWalker
+from pygwalker.services.data_parsers import get_parser
+from pygwalker.services.preview_image import render_gw_chart_preview_html
 from pygwalker.data_parsers.base import FieldSpec
+from pygwalker.data_parsers.database_parser import Connector
 from pygwalker._typing import DataFrame
 from pygwalker.utils.randoms import generate_hash_code
 from pygwalker.utils.check_walker_params import check_expired_params
@@ -71,3 +74,45 @@ def to_html(
         logger.error(traceback.format_exc())
         return f"<div>{str(e)}</div>"
     return html
+
+
+def to_chart_html(
+    dataset: Union[DataFrame, Connector, str],
+    spec: Dict[str, Any],
+    *,
+    spec_type: Literal["graphic-walker", "vega"] = "graphic-walker",
+    theme_key: Literal['vega', 'g2'] = 'g2',
+    dark: Literal['media', 'light', 'dark'] = 'media',
+) -> str:
+    """
+    Generate HTML code of a chart by graphic-walker or vega spec.
+
+    Args:
+        - dataset (pl.DataFrame | pd.DataFrame | Connector, optional): dataset.
+        - spec (Dict[str, Any]): chart config data.
+
+    Kargs:
+        - spec_type (Literal["graphic-walker", "vega"]): type of spec.
+        - theme_key ('vega' | 'g2'): theme type.
+        - dark ('media' | 'light' | 'dark'): 'media': auto detect OS theme.
+    """
+    # pylint: disable=import-outside-toplevel
+    # Since the compatibility of quick js is not certain, the related methods are lazy loaded.
+    from pygwalker.utils.dsl_transform import vega_to_dsl, dsl_to_workflow
+
+    data_parser = get_parser(dataset)
+    if spec_type == "vega":
+        gw_dsl = vega_to_dsl(spec, data_parser.raw_fields)
+    else:
+        gw_dsl = spec
+    workflow = dsl_to_workflow(gw_dsl)
+
+    data = data_parser.get_datas_by_payload(workflow)
+    return render_gw_chart_preview_html(
+        single_vis_spec=gw_dsl,
+        data=data,
+        theme_key=theme_key,
+        dark=dark,
+        title="",
+        desc=""
+    )
