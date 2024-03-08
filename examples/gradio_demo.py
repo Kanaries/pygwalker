1dc0a2ddd6c7beb4634cb96fc2456d86e85b3fb7
longxiaofei <libra.19951002@gmail.com>
2024-03-08 16:15:00 +0800

    docs: update example

--- examples/gradio_demo.py
+++ examples/gradio_demo.py
@@ -5,7 +5,7 @@ from pygwalker.api.gradio import PYGWALKER_ROUTE, get_html_on_gradio
 
 with gr.Blocks() as demo:
     df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
-    pyg_html = get_html_on_gradio(df, spec="/Users/douding/Desktop/github/pygwalker-in-streamlit/gw_config.json", debug=True)
+    pyg_html = get_html_on_gradio(df, spec="/Users/douding/Desktop/github/pygwalker-in-streamlit/gw_config.json", spec_io_mode="rw")
     gr.HTML(pyg_html)
 
 
