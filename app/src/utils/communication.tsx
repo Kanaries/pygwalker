cee6f8c9e074471120ca3837c9b52670f46ed437
Thomas Bouamoud <thomasbs17@yahoo.fr>
2024-05-31 10:39:07 +0100

    bug fix to retrieve specs on URLs with path

--- app/src/utils/communication.tsx
+++ app/src/utils/communication.tsx
@@ -172,7 +172,7 @@ const initHttpCommunication = (gid: string, baseUrl: string) => {
     if (domain === "streamlit.app") {
         url = `/~/+/_stcore/_pygwalker/comm/${gid}`
     } else {
-        url = `/${baseUrl}/${gid}`
+        url = `${window.parent.document.href}/${baseUrl}/${gid}`
     }
 
     const sendMsg = async(action: string, data: any, timeout: number = 30_000) => {
