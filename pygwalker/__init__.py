feb10f569cc1e138b80ec5ac62efe7c756b26fb5
longxiaofei <libra.19951002@gmail.com>
2024-07-23 17:55:23 +0800

    feat: add component api

--- pygwalker/__init__.py
+++ pygwalker/__init__.py
@@ -10,12 +10,13 @@ from pygwalker.utils.execute_env_check import check_kaggle as __check_kaggle
 from pygwalker.services.global_var import GlobalVarManager
 from pygwalker.services.kaggle import show_tips_user_kaggle as __show_tips_user_kaggle
 
-__version__ = "0.4.9.1"
+__version__ = "0.4.9.2"
 __hash__ = __rand_str()
 
 from pygwalker.api.jupyter import walk, render, table
 from pygwalker.api.html import to_html
 from pygwalker.data_parsers.base import FieldSpec
+from pygwalker.api.component import component
 
 if GlobalVarManager.privacy == 'offline':
     logging.getLogger(__name__).info("Running in offline mode. There might be newer releases available. Please check at https://github.com/Kanaries/pygwalker or https://pypi.org/project/pygwalker.")
@@ -23,4 +24,4 @@ if GlobalVarManager.privacy == 'offline':
 if __check_kaggle():
     __show_tips_user_kaggle()
 
-__all__ = ["walk", "render", "table", "to_html", "FieldSpec", "GlobalVarManager"]
+__all__ = ["walk", "render", "table", "to_html", "FieldSpec", "GlobalVarManager", "component"]
