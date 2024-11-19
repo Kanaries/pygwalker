964223d66fa5b82364c94f97946cfe9ec05db98d
Bruk07 <asratbruk16@gmail.com>
2024-11-19 16:19:48 +0100

    Add doc-string and reorder import statements

--- pygwalker/services/check_update.py
+++ pygwalker/services/check_update.py
@@ -1,14 +1,23 @@
-from typing import Dict, Any, Coroutine
-from urllib import request
-from threading import Thread
+"""
+Check_Update Module: This module enables you to check if
+the version is up-to-date.
+
+Updated on Tue November 11 15:15:48 2024
+
+@author: Kanaries
+
+"""
+
 import asyncio
 import logging
 import sys
 import json
-
-from .config import get_local_user_id
+from typing import Dict, Any, Coroutine
+from urllib import request
+from threading import Thread
 from pygwalker import __version__
 from pygwalker.services.global_var import GlobalVarManager
+from .config import get_local_user_id
 
 
 _UPDATE_URL = 'https://5agko11g7e.execute-api.us-west-1.amazonaws.com/default/check_updates'
@@ -17,6 +26,15 @@ logger = logging.getLogger(__name__)
 
 
 def _sync_get_async_result(co: Coroutine[Any, Any, Any]) -> Any:
+    """
+    fetch asynchronous result.
+
+    Parameters
+    ----------
+    co : Coroutine
+        asynchronous parameter.
+
+    """
     loop = asyncio.new_event_loop()
     try:
         return loop.run_until_complete(co)
@@ -25,17 +43,53 @@ def _sync_get_async_result(co: Coroutine[Any, Any, Any]) -> Any:
 
 
 async def _request_on_pyodide(url: str) -> Dict[str, Any]:
+    """
+    request url.
+
+    Parameters
+    ----------
+    url : string
+        url string.
+    Returns
+    ---------
+    Dict
+
+    """
     import pyodide
     resp = await pyodide.http.pyfetch(url)
     return await resp.json()
 
 
 def _request_on_python(url: str) -> Dict[str, Any]:
+    """
+    request url on python.
+
+    Parameters
+    ----------
+    url : string
+        url string.
+    Returns
+    ---------
+    Dict
+
+    """
     with request.urlopen(url, timeout=30) as resp:
         return json.loads(resp.read().decode("utf-8"))
 
 
 def _check_update() -> Dict[str, Any]:
+    """
+    Internal function to Check for updates.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    ---------
+    Dict
+
+    """
     payload = {'pkg': 'pygwalker', 'v': __version__, 'hashcode': get_local_user_id()}
     request_func = _request_on_python
 
@@ -51,10 +105,18 @@ def _check_update() -> Dict[str, Any]:
         if isinstance(result, Coroutine):
             result = _sync_get_async_result(result)
         return result
-    except:
+    finally:
         pass
 
 
 def check_update() -> None:
+    """
+    Check for update.
+
+    Parameters
+    ----------
+    None
+
+    """
     if GlobalVarManager.privacy != "offline":
         Thread(target=_check_update).start()
