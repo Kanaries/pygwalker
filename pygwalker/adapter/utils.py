3566bce50dceeb6be813962b4e1db775271ccd23
islxyqwe <islxyqwe123@gmail.com>
2024-12-02 20:23:30 +0800

    fix: detect in google colab

--- pygwalker/adapter/utils.py
+++ pygwalker/adapter/utils.py
@@ -1,11 +1,14 @@
 def is_notebook() -> bool:
     try:
-        shell = get_ipython().__class__.__name__
+        ipython = get_ipython()
+        shell = ipython.__class__.__name__
         if shell == 'ZMQInteractiveShell':
             return True   # Jupyter notebook or qtconsole
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
         else:
+            if 'google.colab' in str(ipython):
+                return True # in Google colab
             return False  # Other type (?)
     except NameError:
         return False      # Probably standard Python interpreter
