836e14896fbc4ca023131c2a9386b8718a2b3170
islxyqwe <islxyqwe123@gmail.com>
2024-12-10 15:48:30 +0800

    temp2

--- pygwalker/renderers/pyplot.py
+++ pygwalker/renderers/pyplot.py
@@ -1,7 +1,9 @@
 import base64
 import io
 import json
-from typing import List
+from typing import Dict
+
+from pygwalker._typing import DataFrame
 from pygwalker.renderers.base import CodeStorage, auto_mark, get_color_palette, get_fid_with_agg, get_name_with_agg, get_primary_color, get_spec_color_palette, pick_first
 import matplotlib
 from matplotlib.colors import LinearSegmentedColormap
@@ -10,8 +12,9 @@ import matplotlib.pyplot as plt
 import itertools
 import matplotlib.patches as mpatches
 
-def render_image(df, payload, size):
+def build_code(payload: Dict[str, any], size: Dict[str, int], df_name = 'df'):
     code_storage = CodeStorage()
+    code_storage.add_code(f"import matplotlib.pyplot as plt")
     config = payload.get("config")
     mark = config.get("geoms")[0]
     is_aggergated = config.get("defaultAggregated")
@@ -55,13 +58,13 @@ def render_image(df, payload, size):
             if color_palette is None:
                 color_palette = get_color_palette('blues')
             code_storage.add_code(f"color_map = LinearSegmentedColormap.from_list('palette',{json.dumps(color_palette)})")
-            color_string = f"c=df['{fid_color}'],cmap=color_map, "
+            color_string = f"c={df_name}['{fid_color}'],cmap=color_map, "
             colorbar_string = 'plt.colorbar()'
         else:
             if color_palette is None:
                 color_palette = get_color_palette('category10')
             code_storage.add_code(f"cp = {json.dumps(color_palette)}")
-            code_storage.add_code(f"colors = df['{fid_color}']")
+            code_storage.add_code(f"colors = {df_name}['{fid_color}']")
             code_storage.add_code("unique_color = colors.unique()")
             code_storage.add_code("color_map = dict(zip(unique_color, itertools.cycle(cp)))")
             code_storage.add_code("c = colors.map(color_map)")
@@ -75,19 +78,18 @@ def render_image(df, payload, size):
         fid_x = get_fid_with_agg(x_channels[0], is_aggergated)
         fid_y = get_fid_with_agg(y_channels[0], is_aggergated)
         
-        x_string = f"df['{fid_x}'], " if fid_x else ''
-        y_string = f"df['{fid_y}'], " if fid_y else ''
+        x_string = f"{df_name}['{fid_x}'], " if fid_x else ''
+        y_string = f"{df_name}['{fid_y}'], " if fid_y else ''
         
-        size_string = f"{'width' if mark == 'bar' else 's'}=df['{fid_size}'], " if fid_size else ''
+        size_string = f"{'width' if mark == 'bar' else 's'}={df_name}['{fid_size}'], " if fid_size else ''
 
-        # code_storage.add_code(f"df.plot({x_string}{y_string}{color_string}{opacity_string}{size_string}{plot_func})")
         code_storage.add_code(f"plt.{plot_func}({x_string}{y_string}{color_string}{opacity_string}{size_string}{plot_options})")
         if colorbar_string:
             code_storage.add_code(colorbar_string)
         code_storage.add_code(f"plt.xlabel('{get_name_with_agg(x_channels[0], is_aggergated)}')")
         code_storage.add_code(f"plt.ylabel('{get_name_with_agg(y_channels[0], is_aggergated)}')")
         if fid_text:
-            code_storage.add_code(f"for i, txt in enumerate(df['{fid_text}']):" + f"\n    plt.text(df['{fid_x}'][i], df['{fid_y}'][i], txt)")
+            code_storage.add_code(f"plt.text({df_name}['{fid_x}'], {df_name}['{fid_y}'], {df_name}['{fid_text}'])")
     
     def plot_box_single():
         fid_y = get_fid_with_agg(pick_first(y_channels), is_aggergated)
@@ -95,27 +97,21 @@ def render_image(df, payload, size):
         name_x = get_name_with_agg(pick_first(x_channels), is_aggergated)
         name_y = get_name_with_agg(pick_first(y_channels), is_aggergated)
         if fid_y is None and fid_x is not None:
-            code_storage.add_code(f"plt.boxplot(df['{fid_x}'], patch_artist=True, vert=False, labels=['{name_x}'])")
+            code_storage.add_code(f"plt.boxplot({df_name}['{fid_x}'], patch_artist=True, vert=False, labels=['{name_x}'])")
             
         elif fid_x is None and fid_y is not None:
-            code_storage.add_code(f"plt.boxplot(df['{fid_y}'], patch_artist=True, labels=['{name_y}'])")
+            code_storage.add_code(f"plt.boxplot({df_name}['{fid_y}'], patch_artist=True, labels=['{name_y}'])")
         else:
-            code_storage.add_code(f"values = df['{fid_y}'].groupby(df['{fid_x}'])")
+            code_storage.add_code(f"values = {df_name}['{fid_y}'].groupby({df_name}['{fid_x}'])")
             code_storage.add_code(f"plt.boxplot([group[1] for group in values], patch_artist=True, labels=[group[0] for group in values])")
             
     def plot_arc_single_only():
         theta_channel = encodings.get("theta")
-        radius_channel = encodings.get("radius")
         fid_theta = get_fid_with_agg(pick_first(theta_channel), is_aggergated)
-        fid_radius = get_fid_with_agg(pick_first(radius_channel), is_aggergated)
-        
-        if fid_radius is None:
-            label_string = f"labels=df['{fid_color}'], " if fid_color else ''
-            code_storage.add_code(f"plt.pie(df['{fid_theta}'], autopct='%1.1f%%', {label_string})")
-        else:
-            # df['fid_theta'] is [1,1,1]
-            code_storage.add_code(f"ax = plt.subplot(projection='polar')")
-            code_storage.add_code(f"ax.bar(df['{fid_theta}'], df['{fid_radius}'])")
+        label_string = f"labels={df_name}['{fid_color}'], " if fid_color else ''
+        code_storage.add_code(f"plt.pie({df_name}['{fid_theta}'], autopct='%1.1f%%', {label_string}{color_string})")
+        if colorbar_string:
+            code_storage.add_code(colorbar_string)
 
 
     plot_types = {
@@ -140,22 +136,29 @@ def render_image(df, payload, size):
         if len(x_channels) == 1 and len(y_channels) == 1:
             plot_single(plot_types[mark], plot_options.get(mark, ''))
         else:
+            # TODO: plot_box_multiple()
             # plot_multiple(plot_types[mark])
             return ''
             pass
     else:
         if mark == 'boxplot':
-            plot_box_single()
+            if len(x_channels) == 1 and len(y_channels) == 1:
+                plot_box_single()
+            else:
+                # TODO: plot_box_multiple()
+                pass
         elif mark == 'arc':
             plot_arc_single_only()
         else:
             return ''
-        pass
+    return code_storage.output_all()
+
+
+def render_image(df: DataFrame, payload: Dict[str, any], size: Dict[str, int]):
+    code = build_code(payload, size)
     with plt.ioff():
-        code = code_storage.output_all()
         print(code)
         exec(code)
-        code_storage.add_code("plt.show()")
         my_stringIObytes = io.BytesIO()
         plt.savefig(my_stringIObytes, format='png')
         plt.clf()
@@ -164,9 +167,5 @@ def render_image(df, payload, size):
         my_stringIObytes.seek(0)
         my_base64_pngData = base64.b64encode(my_stringIObytes.read()).decode()
 
-    
     return json.dumps({"image":  "data:image/png;base64," + my_base64_pngData, "size": size})
 
-
-def linearmap_color(colorlist: List[str]):
-    return LinearSegmentedColormap.from_list('custom', colorlist)
