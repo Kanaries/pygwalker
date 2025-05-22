6eb1053f46fa1263a719a990ca41074dcf424174
observedobserver <270001151@qq.com>
2025-05-23 01:21:51 +0800

    feat: reflex api

---
+++ examples/rxconfig.py
@@ -0,0 +1,8 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="reflex_demo",
+    backend_port=8000,
+    loglevel="debug",
+    module_name="reflex_demo"  # Use the original file name
+)
