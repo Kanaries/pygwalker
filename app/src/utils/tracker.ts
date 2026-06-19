82eeaa379173c279f714f37234407c2c9bbb485e
observedobserver <270001151@qq.com>
2026-06-18 21:13:43 -0700

    fix: make frontend telemetry opt-in state idempotent

--- app/src/utils/tracker.ts
+++ app/src/utils/tracker.ts
@@ -2,24 +2,29 @@ import { AnalyticsBrowser } from '@segment/analytics-next'
 
 
 const initTracker = () => {
-    var userId = "";
-    var open = false;
-    var analytics;
+    let userId = "";
+    let open = false;
+    let analytics: ReturnType<typeof AnalyticsBrowser.load> | undefined;
 
     const setUserId = (id: string) => {
         userId = id;
     };
 
     const setOpen = (value: boolean) => {
-        if (value) {
-            analytics = AnalyticsBrowser.load({ writeKey: '9gxTNvl6Pl4WaZ5aymeHEBqNN8K4Op0U' })
+        if (value === open) {
+            return;
         }
         open = value;
+        if (!open) {
+            analytics = undefined;
+            return;
+        }
+        analytics = AnalyticsBrowser.load({ writeKey: '9gxTNvl6Pl4WaZ5aymeHEBqNN8K4Op0U' });
     }
 
     const track = (eventName: string, data: any) => {
-        if (open) {
-            analytics.track(eventName, {...data, userId})
+        if (open && analytics) {
+            analytics.track(eventName, {...data, userId});
         }
     };
 
